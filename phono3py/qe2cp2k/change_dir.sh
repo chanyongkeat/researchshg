de5e00ba866687838f2bc460b936d10765c1efb7
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-06 02:40:50 +0800

    Create change_dir.sh

---
+++ phono3py/qe2cp2k/change_dir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Loop through the desired range of indices
+for i in {00001..00003}; do  # Change 3 to the desired upper limit
+    # Create the directory disp-$i
+    dir_name="disp-$i"
+    mkdir -p "$dir_name"
+
+    # Copy coord$i.in to the disp-$i directory
+    cp "/home/cyk/Phono3py/convert/coord$i.in" "$dir_name/"
+
+    # Rename coord$i.in to coord.in within the disp-$i directory
+    mv "$dir_name/coord$i.in" "$dir_name/coord.in"
+
+done

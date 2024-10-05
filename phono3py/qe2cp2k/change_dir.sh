e675dc081ff8d6e45ea6ab184cc4967521035cfb
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-06 02:49:24 +0800

    Update change_dir.sh

--- phono3py/qe2cp2k/change_dir.sh
+++ phono3py/qe2cp2k/change_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop through the desired range of indices
-for i in {00001..00003}; do  # Change 3 to the desired upper limit
+for i in {00001..00003}; do  # Adjust the upper limit as needed
     # Create the directory disp-$i
     dir_name="disp-$i"
     mkdir -p "$dir_name"
@@ -12,5 +12,12 @@ for i in {00001..00003}; do  # Change 3 to the desired upper limit
     # Rename coord$i.in to coord.in within the disp-$i directory
     mv "$dir_name/coord$i.in" "$dir_name/coord.in"
 
-    cd "$dir_name" && wget https://raw.githubusercontent.com/chanyongkeat/researchshg/refs/heads/main/phono3py/qe2cp2k/supercell.inp
+    # Change to the disp-$i directory
+    cd "$dir_name" 
+
+    # Download the supercell.inp file
+    wget https://raw.githubusercontent.com/chanyongkeat/researchshg/refs/heads/main/phono3py/qe2cp2k/supercell.inp
+
+    # Change back to the original directory (optional)
+    cd ..
 done

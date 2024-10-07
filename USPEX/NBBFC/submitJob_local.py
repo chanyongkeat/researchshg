3247f436bdd1381ea7dbe0a785c261f1aec86b79
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-07 17:28:58 +0800

    Create submitJob_local.py

---
+++ USPEX/NBBFC/submitJob_local.py
@@ -0,0 +1,63 @@
+from __future__ import with_statement
+from __future__ import absolute_import
+from subprocess import check_output
+import re
+import sys
+from io import open
+
+
+
+def submitJob_local(index, commandExecutable):
+    """
+    This routine is to submit job locally
+    One needs to do a little edit based on your own case.
+
+    Step 1: to prepare the job script which is required by your supercomputer
+    Step 2: to submit the job with the command like qsub, bsub, llsubmit, .etc.
+    Step 3: to get the jobID from the screen message
+    :return: job ID
+    """
+
+    RUN_FILENAME = 'myrun'
+    JOB_NAME = 'USPEX-{}'.format(index)
+
+    # Step 1
+    myrun_content = '''#!/bin/sh
+#PBS -N {}
+#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=18
+#PBS -q workq
+
+JOBID=`echo $PBS_JOBID | cut -d '.' -f 1`
+
+cd $PBS_O_WORKDIR
+
+{}
+'''.format(JOB_NAME, commandExecutable)
+
+
+    with open(RUN_FILENAME, 'w') as fp:
+        fp.write(myrun_content)
+
+    # Step 2
+    # It will output some message on the screen like '2350873.nano.cfn.bnl.local'
+    output = check_output('qsub {}'.format(RUN_FILENAME), shell=True, universal_newlines=True)
+
+
+    # Step 3
+    # Here we parse job ID from the output of previous command
+    jobNumber = int(output.split('.')[0])
+    print(str(jobNumber))
+    return jobNumber
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', dest='index', type=int)
+    parser.add_argument('-c', dest='commandExecutable', type=str)
+    args = parser.parse_args()
+
+    jobNumber = submitJob_local(index=args.index, commandExecutable=args.commandExecutable)
+    print('<CALLRESULT>')
+    print(int(jobNumber))

88b1e27ffb75bdb44d86c711e3f649a0843959c8
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-18 23:32:43 +0800

    Update cp2k.sh

--- Phonopy/NBBFC/cp2k.sh
+++ Phonopy/NBBFC/cp2k.sh
@@ -2,5 +2,5 @@
 
 for i in $(seq -w 001 234);
 do
-  sudo docker run -it --rm --shm-size=1g -v $PWD:/mnt -u $(id -u $USER):$(id -g $USER) cp2k/cp2k:2024.3_mpich_generic_psmp mpirun -n 90 -genv OMP_NUM_THREADS=2 cp2k -i NBBFC-supercell-$i.inp > output/NBBFC-supercell-$i.out;
+  docker run --rm --shm-size=1g -v $PWD:/mnt -u $(id -u $USER):$(id -g $USER) cp2k/cp2k:2024.3_mpich_generic_psmp mpirun -n 90 -genv OMP_NUM_THREADS=2 cp2k -i NBBFC-supercell-$i.inp > output/NBBFC-supercell-$i.out;
 done

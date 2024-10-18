ccaf1af008ca3d0d9636d6d809427f45e35ae330
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-18 22:39:58 +0800

    Update pwrun.sh

--- phono3py/QE_run/pwrun.sh
+++ phono3py/QE_run/pwrun.sh
@@ -3,5 +3,5 @@
 for i in {00001..00111};
 do
   cat header.in supercell/supercell-$i.in > input/Si-$i.in;
-  mpirun -np 4 pw.x -inp input/Si-$i.in > output/Si-$i.out
+  mpirun -np 4 pw.x -inp input/Si-$i.in > output/Si-$i.out;
 done

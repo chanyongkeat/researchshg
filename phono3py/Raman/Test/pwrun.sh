199dbb60e598091c1a5a7a1e0bd385c238502248
Chan Yong Keat <150885322+chanyongkeat@users.noreply.github.com>
2024-10-28 14:10:01 +0800

    Create pwrun.sh

---
+++ phono3py/Raman/Test/pwrun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=2
+
+for i in {00001..00111};
+do
+  cat header.in supercell/supercell-$i.in > input/Si-$i.in;
+  mpirun -np 4 pw.x -inp input/Si-$i.in > output/Si-$i.out;
+done
